01165a087ab191ada3b471a888de7c06cc20d308
mmk0612 <mihirmilindkumar_it21a9_62@dtu.ac.in>
2023-09-03 17:00:45 +0530

    Delhi Rentals project to push

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+data = pd.read_csv('Cleaned_data.csv')
+pipe = pickle.load(open('RidgeModel.pkl', 'rb'))
+
+
+@app.route('/')
+def index():
+    localityNames = sorted(data['localityName'].unique())
+    districts = sorted(data['suburbName'].unique())
+    return render_template('index.html', localityNames=localityNames, districts=districts)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    localityName = request.form.get('localityName')
+    bedrooms = request.form.get('bedrooms')
+    size_sq_ft = request.form.get('size')
+    input = pd.DataFrame([[localityName, bedrooms, size_sq_ft]], columns=[
+                         'localityName', 'bedrooms', 'size_sq_ft'])
+    prediction = pipe.predict(input)[0]
+    return str(prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
